939c78eb9732d51f3747c365e90c3f0a7fa886c4
bdbcat <bdbcat>
2009-06-03 03:08:22 +0000

    1.3.1

--- src/about.cpp
+++ src/about.cpp
@@ -1,5 +1,5 @@
 /******************************************************************************
- * $Id: about.cpp,v 1.17 2009/05/05 03:54:48 bdbcat Exp $
+ * $Id: about.cpp,v 1.18 2009/06/03 03:08:22 bdbcat Exp $
  *
  * Project:  OpenCPN
  * Purpose:  About Dialog
@@ -27,6 +27,9 @@
  *
  *
  * $Log: about.cpp,v $
+ * Revision 1.18  2009/06/03 03:08:22  bdbcat
+ * 1.3.1
+ *
  * Revision 1.17  2009/05/05 03:54:48  bdbcat
  * Version 1.3.0
  *
@@ -87,12 +90,12 @@
 #include "about.h"
 #include "chart1.h"
 
-CPL_CVSID("$Id: about.cpp,v 1.17 2009/05/05 03:54:48 bdbcat Exp $");
+CPL_CVSID("$Id: about.cpp,v 1.18 2009/06/03 03:08:22 bdbcat Exp $");
 
 
 //    Some constants
 
-char OpenCPNVersion[] = {"\n\n                      Version 1.3.0"};
+char OpenCPNVersion[] = {"\n\n                      Version 1.3.1"};
 
 
 char AboutText[] =
